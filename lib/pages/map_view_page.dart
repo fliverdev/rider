12ee8a88b997bcc801f1147fefd5412a85854f5b
urmilshroff <urmilshroff@gmail.com>
2019-08-19 17:28:39 +0530

    Trying to write to DB

--- lib/pages/map_view_page.dart
+++ lib/pages/map_view_page.dart
@@ -1,12 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:rider/utils/functions.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:rider/services/firestore_crud.dart';
+import 'package:rider/utils/functions.dart';
 
 class MyMapViewPage extends StatefulWidget {
   @override
@@ -15,16 +13,11 @@ class MyMapViewPage extends StatefulWidget {
 
 class _MyMapViewPageState extends State<MyMapViewPage> {
   var currentLocation;
-  var firestoreCrudObj = new FirestoreCrud();
-//  var currentLocation;
   var clients = [];
+  var firestoreCrudObj = new FirestoreCrud();
+  QuerySnapshot items;
   GoogleMapController mapController;
   Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
-  QuerySnapshot items;
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
 
   void initState() {
     super.initState();
@@ -33,16 +26,22 @@ class _MyMapViewPageState extends State<MyMapViewPage> {
         currentLocation = currLoc;
       });
     });
-    firestoreCrudObj.getData().then((QuerySnapshot results) {
-      setState(() {
-        items = results;
-      });
-    });
+//    firestoreCrudObj.getData().then(
+//      (QuerySnapshot results) {
+//        setState(() {
+//          items = results;
+//        });
+//      },
+//    );
   } // gets current user location when the app loads
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   populateClients() {
     clients = [];
-    Firestore.instance.collection('markers').getDocuments().then((docs) {
+    Firestore.instance.collection('markers2').getDocuments().then((docs) {
       if (docs.documents.isNotEmpty) {
         for (int i = 0; i < docs.documents.length; i++) {
           clients.add(docs.documents[i].data);
@@ -59,52 +58,6 @@ class _MyMapViewPageState extends State<MyMapViewPage> {
       ),
     );
   } // cool animation when zooming into the user location
-
-//  initMarker(client) {
-//    mapController.clearMarkers().then((val) {
-//      mapController.addMarker(
-//        MarkerOptions(
-//          position: LatLng(latitude,)
-//        )
-//
-//      );
-//    });
-//  }
-
-//Geolocator Function
-//  void getLocation() async {
-//    Position position = await Geolocator()
-//        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-//    print(position);
-//  }
-
-//  static var pos;
-//  var location = new Location();
-//  var currentLocation = LocationData;
-//  //To detect location - 13/8/19 (added)
-//  _animateToUser() async {
-//    currentLocation = await location.getLocation();
-//    mapController.animateCamera(
-//      CameraUpdate.newCameraPosition(
-//        CameraPosition(
-//          target: _center,
-//          zoom: 15.0,
-//        ),
-//      ),
-//    );
-//  }
-
-//  location.onLocationChanged().listen((LocationData currentLocation) {
-//  print(currentLocation.latitude);
-//  print(currentLocation.longitude);
-//  });
-
-//  LocationData currentLocation = new LocationData();
-//  final double latitude = location.latitude;
-//  final double longitude = location.long
-//  final LatLng _center = const LatLng(18.9548, 72.7985); //malabar hill
-//  final LatLng _center = LatLng(currentLocation['latitude'],
-//      currentLocation['langitude']); //to detect current location
 
   @override
   Widget build(BuildContext context) {
@@ -159,8 +112,11 @@ class _MyMapViewPageState extends State<MyMapViewPage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   RaisedButton(
-                    child: Text('Save Location'),
-                    onPressed: doNothing,
+                    child: Text('Write'),
+                    onPressed: () {
+                      Map locationData = {'coordinates': 12345};
+                      firestoreCrudObj.addDataVideo(locationData);
+                    },
                   ),
                   SizedBox(
                     width: 10.0,
@@ -182,11 +138,6 @@ class _MyMapViewPageState extends State<MyMapViewPage> {
           ],
         ),
       ),
-//      floatingActionButton: FloatingActionButton(
-//          child: Icon(Icons.my_location),
-//          foregroundColor: invertInvertColorsTheme(context),
-//          backgroundColor: invertColorsTheme(context),
-//          onPressed: doNothing),
     );
   }
 }
--- lib/services/firestore_crud.dart
+++ lib/services/firestore_crud.dart
@@ -1,22 +1,30 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:rider/utils/functions.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:location/location.dart';
-import 'package:flutter/services.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class FirestoreCrud {
-  Future<void> addData(data) async {
-    Firestore.instance.collection('markers').add(data).catchError((error) {
+  Future<void> addDataVideo(data) async {
+    Firestore.instance.collection('markers2').add(data).catchError((error) {
       print(error);
     });
+  } //writes data to firestore
+
+  addData(data) {
+    try {
+      Firestore.instance.runTransaction(
+        (Transaction transaction) async {
+          await Firestore.instance
+              .collection('markers2')
+              .document()
+              .setData(data.toJson());
+        },
+      );
+    } catch (e) {
+      print(e.toString());
+    }
   }
 
   getData() async {
-    return await Firestore.instance.collection('markers').getDocuments();
-  }
+    return await Firestore.instance.collection('markers2').getDocuments();
+  } //reads data from firestore
 }
