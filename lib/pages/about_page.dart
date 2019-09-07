4228c10860b5aa304e137b0fee1bfae839eb1680
urmilshroff <urmilshroff@gmail.com>
2019-09-08 01:56:25 +0530

    Recreated about page

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:rider/utils/colors.dart';
 import 'package:rider/utils/ui_helpers.dart';
 import 'package:rider/widgets/sexy_tile.dart';
 
@@ -12,6 +12,24 @@ class MyAboutPage extends StatefulWidget {
 class _MyAboutPageState extends State<MyAboutPage> {
   @override
   Widget build(BuildContext context) {
+    List<String> contributorNames = [
+      'Urmil Shroff',
+      'Priyansh Ramnani',
+      'Vinay Kolwankar',
+    ];
+
+    List<String> contributorDesc = [
+      'Developer',
+      'Developer',
+      'Designer',
+    ];
+
+    List<String> contributorPhotos = [
+      'urmil.jpg',
+      'priyansh.jpg',
+      'vinay.png',
+    ];
+
     return Scaffold(
       backgroundColor: invertInvertColorsStrong(context),
       body: Container(
@@ -46,164 +64,92 @@ class _MyAboutPageState extends State<MyAboutPage> {
                 ],
               ),
             ),
+//            Expanded(
+//              child: GridView.count(
+//                crossAxisCount: 1,
+//                childAspectRatio: 2,
+//                children: <Widget>[
+//                  SexyTile(
+//                    child: Column(
+//                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                      crossAxisAlignment: CrossAxisAlignment.center,
+//                      children: <Widget>[
+//                        Container(
+//                          width: 110.0,
+//                          height: 110.0,
+//                          decoration: BoxDecoration(
+//                            image: DecorationImage(
+//                                image: isThemeCurrentlyDark(context)
+//                                    ? AssetImage(
+//                                        './assets/images/fliver-green.png')
+//                                    : AssetImage(
+//                                        './assets/images/fliver-black.png')),
+//                          ),
+//                        )
+//                      ],
+//                    ),
+//                    splashColor: MyColors.primaryColor,
+//                    onTap: () => print('Fliver tapped'),
+//                  ),
+//                ],
+//              ),
+//            ),
             Expanded(
-              child: StaggeredGridView.count(
+              child: GridView.count(
                 crossAxisCount: 1,
-                children: <Widget>[
-                  sexyTile(
-                    context,
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          width: 110.0,
-                          height: 110.0,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: isThemeCurrentlyDark(context)
-                                    ? AssetImage(
-                                        './assets/images/fliver-green.png')
-                                    : AssetImage(
-                                        './assets/images/fliver-black.png')),
+                childAspectRatio: 1.75,
+                children: List.generate(
+                  contributorNames.length,
+                  (i) {
+                    return SexyTile(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          SizedBox(
+                            height: 5.0,
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(
-                            left: 15.0,
-                            right: 15.0,
+                          Container(
+                            width: 70.0,
+                            height: 70.0,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                image: AssetImage(
+                                    './assets/images/${contributorPhotos[i]}'),
+                              ),
+                            ),
                           ),
-                          child: Text(
-                            'Fliver is an app developed by a bunch of '
-                            'developers and designers who believe in open'
-                            ' source software.',
+                          Text(
+                            '${contributorNames[i]}',
                             style: TextStyle(
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w400,
+                              fontSize: 20.0,
                               color: invertColorsStrong(context),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(
-                            left: 15.0,
-                            right: 15.0,
-                          ),
-                          child: Text(
-                            'We\'re also part-time Computer Engineering '
-                            'students, so yeah.',
+                          Text(
+                            '${contributorDesc[i]}',
                             style: TextStyle(
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w400,
+                              fontSize: 16.0,
                               fontStyle: FontStyle.italic,
                               color: invertColorsStrong(context),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                      ],
-                    ),
-                    onTap: doNothing,
-                  ),
-                  sexyTile(
-                    context,
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          width: 75.0,
-                          height: 75.0,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                              image: AssetImage('./assets/images/urmil.png'),
-                            ),
+                          SizedBox(
+                            height: 5.0,
                           ),
-                        ),
-                        Text(
-                          'Urmil Shroff',
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            color: invertColorsStrong(context),
-                          ),
-                        ),
-                      ],
-                    ),
-                    onTap: doNothing,
-                  ),
-                  sexyTile(
-                    context,
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          width: 75.0,
-                          height: 75.0,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                              image:
-                                  AssetImage('./assets/images/priyansh.jpeg'),
-                            ),
-                          ),
-                        ),
-                        Text(
-                          'Priyansh Ramnani',
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            color: invertColorsStrong(context),
-                          ),
-                        ),
-                      ],
-                    ),
-                    onTap: doNothing,
-                  ),
-                  sexyTile(
-                    context,
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          width: 75.0,
-                          height: 75.0,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                              image: AssetImage('./assets/images/vinay.png'),
-                            ),
-                          ),
-                        ),
-                        Text(
-                          'Vinay Kolwankar',
-                          style: TextStyle(
-                            fontSize: 22.0,
-                            color: invertColorsStrong(context),
-                          ),
-                        ),
-                      ],
-                    ),
-                    onTap: doNothing,
-                  ),
-                  SizedBox(), // just to add space at the end
-                ],
-                staggeredTiles: [
-                  StaggeredTile.extent(1, 275.0),
-                  StaggeredTile.extent(1, 200.0),
-                  StaggeredTile.extent(1, 200.0),
-                  StaggeredTile.extent(1, 200.0),
-                  StaggeredTile.extent(1, 50.0), // for the SizedBox
-                ],
+                        ],
+                      ),
+                      splashColor: MyColors.primaryColor,
+                      onTap: () => print('${contributorNames[i]} tapped'),
+                    );
+                  },
+                ),
               ),
             ),
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         heroTag: 'fab',
         tooltip: 'View source code',
@@ -212,7 +158,6 @@ class _MyAboutPageState extends State<MyAboutPage> {
         elevation: 5.0,
         child: Icon(
           Icons.code,
-//          size: 36.0,
         ),
         onPressed: doNothing,
       ),
