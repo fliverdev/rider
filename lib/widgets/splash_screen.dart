b3d8dec6b657fc5dfe630dbdf76f0e2ae5e1b7c5
urmilshroff <urmilshroff@gmail.com>
2019-10-24 19:38:52 +0530

    Added a much-deserved loading animation

--- lib/widgets/splash_screen.dart
+++ lib/widgets/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:rider/utils/colors.dart';
 import 'package:rider/utils/ui_helpers.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -6,15 +8,31 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: invertInvertColorsStrong(context),
-      child: Center(
-        child: Container(
-          width: 225.0,
-          height: 225.0,
-          child: Image.asset(
-            'assets/other/splash-screen.png',
-            fit: BoxFit.cover,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 225.0,
+                height: 225.0,
+                child: Image.asset(
+                  'assets/other/splash-screen.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              SpinKitDoubleBounce(
+                color: MyColors.primaryColor,
+              ),
+            ],
           ),
-        ),
+        ],
       ),
     );
   }
